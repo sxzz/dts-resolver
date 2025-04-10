982cffa4416df4dba535fa18c65fd954d4b4807a
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-04-10 19:08:27 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { existsSync } from 'node:fs'
+import { ResolverFactory } from 'oxc-resolver'
+import { dirname } from 'pathe'
+
+const RE_JS = /\.[cm]?js$/
+const RE_TS = /\.[cm]?ts$/
+
+// eslint-disable-next-line node/prefer-global/process
+export function createResolver({ cwd = process.cwd() }: { cwd?: string } = {}) {
+  const resolver = new ResolverFactory({
+    mainFields: ['types', 'typings', 'module', 'main'],
+    conditionNames: ['types', 'typings', 'import', 'require'],
+    extensions: ['.d.ts', '.d.mts', '.d.cts', '.ts', '.mts', '.cts'],
+    modules: ['node_modules', 'node_modules/@types'],
+  })
+
+  return (id: string, importer?: string): string | null => {
+    const directory = importer ? dirname(importer) : cwd
+
+    const resolution = resolver.sync(directory, id)
+    if (!resolution.path) return null
+    const resolved = resolution.path
+    if (RE_JS.test(resolved)) {
+      // check dts exists?
+      const dts =
+        tryExists(resolved.replace(RE_JS, '.d.ts')) ||
+        tryExists(resolved.replace(RE_JS, '.d.mts')) ||
+        tryExists(resolved.replace(RE_JS, '.d.cts'))
+      return ensureTs(dts)
+    }
+
+    return ensureTs(resolved)
+  }
+}
+
+function ensureTs(path: string | null): string | null {
+  return path && RE_TS.test(path) ? path : null
+}
+
+function tryExists(path: string): string | null {
+  return existsSync(path) ? path : null
+}
---
+++ tests/index.test.ts
@@ -0,0 +1,74 @@
+import { relative } from 'node:path'
+import process from 'node:process'
+import { describe, expect, test } from 'vitest'
+import { createResolver } from '../src'
+
+const resolver = createResolver()
+
+describe('resolver', () => {
+  test('node modules', () => {
+    expectPath(resolver('node:fs')).toBe(null)
+    expectPath(resolver('node')).toMatchInlineSnapshot(
+      `"@types/node/index.d.ts"`,
+    )
+  })
+
+  test('magic-string-ast', () => {
+    expectPath(resolver('magic-string-ast')).toMatchInlineSnapshot(
+      `"magic-string-ast/dist/index.d.ts"`,
+    )
+    expectPath(
+      resolver('magic-string-ast/dist/index.js'),
+    ).toMatchInlineSnapshot(`"magic-string-ast/dist/index.d.ts"`)
+  })
+
+  test('magic-string', () => {
+    expectPath(resolver('magic-string')).toMatchInlineSnapshot(
+      `"magic-string/dist/magic-string.es.d.mts"`,
+    )
+  })
+
+  test('fast-glob', () => {
+    expectPath(resolver('fast-glob')).toMatchInlineSnapshot(
+      `"fast-glob/out/index.d.ts"`,
+    )
+  })
+
+  test('@babel/*', () => {
+    expectPath(resolver('@babel/parser')).toMatchInlineSnapshot(
+      `"@babel/parser/typings/babel-parser.d.ts"`,
+    )
+    expectPath(resolver('@babel/types')).toMatchInlineSnapshot(
+      `"@babel/types/lib/index-legacy.d.ts"`,
+    )
+  })
+
+  test('@types/*', () => {
+    expectPath(resolver('yargs')).toMatchInlineSnapshot(
+      `"@types/yargs/index.d.mts"`,
+    )
+
+    expectPath(resolver('debug')).toMatchInlineSnapshot(
+      `"@types/debug/index.d.ts"`,
+    )
+  })
+
+  test('vue', () => {
+    expectPath(resolver('vue')).toMatchInlineSnapshot(`"vue/dist/vue.d.mts"`)
+    expectPath(resolver('@vue/reactivity')).toMatchInlineSnapshot(
+      `"@vue/reactivity/dist/reactivity.d.ts"`,
+    )
+  })
+
+  test('not exists', () => {
+    expectPath(resolver('whatever')).toMatchInlineSnapshot(`null`)
+  })
+})
+
+function expectPath(path: string | null) {
+  if (typeof path === 'string')
+    path = relative(process.cwd(), path)
+      .replaceAll('\\', '/')
+      .replaceAll(/.+node_modules\//g, '')
+  return expect(path)
+}
